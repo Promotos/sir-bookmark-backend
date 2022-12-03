69abf3af15e3bf54584e39a5f2176b0b7609712b
Promotos <promotos@gmx.de>
2022-12-03 21:05:04 +0100

    update

--- src/main/kotlin/de/promotos/sirbookmark/SBApplication.kt
+++ src/main/kotlin/de/promotos/sirbookmark/SBApplication.kt
@@ -2,9 +2,19 @@ package de.promotos.sirbookmark
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class DemoApplication
+@RestController
+class DemoApplication {
+
+    @GetMapping("/")
+    fun test() : String {
+        return "ok"
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
