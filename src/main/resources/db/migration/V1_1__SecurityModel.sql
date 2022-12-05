6236fa5d5017d799ae300217d297bae88ff1ec32
Promotos <promotos@gmx.de>
2022-12-05 22:02:14 +0100

    add spring security postgres schema

---
+++ src/main/resources/db/migration/V1_1__SecurityModel.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+	username VARCHAR(50) PRIMARY KEY,
+	password VARCHAR(500) NOT NULL,
+	enabled boolean NOT NULL
+);
+
+CREATE TABLE authorities (
+	username VARCHAR(50) NOT NULL,
+	authority VARCHAR(50) NOT NULL,
+	CONSTRAINT fk_authorities_users FOREIGN KEY(username) REFERENCES users(username)
+);
+CREATE UNIQUE index ix_auth_username ON authorities (username,authority);
