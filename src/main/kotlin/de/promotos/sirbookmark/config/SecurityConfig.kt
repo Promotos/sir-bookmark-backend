422d5555a7f65ccac0d26d75f2be40bceef6ec4d
Promotos <promotos@gmx.de>
2022-12-08 22:13:38 +0100

    pretty print

--- src/main/kotlin/de/promotos/sirbookmark/config/SecurityConfig.kt
+++ src/main/kotlin/de/promotos/sirbookmark/config/SecurityConfig.kt
@@ -66,13 +66,13 @@ class SecurityConfig {
             .username("user")
             .password("{bcrypt}$2a$10\$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
             .roles("USER")
-            .build();
+            .build()
 
         val admin = User.builder()
             .username("admin")
             .password("{bcrypt}$2a$10\$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
             .roles("USER", "ADMIN")
-            .build();
+            .build()
 
         val users = JdbcUserDetailsManager(dataSource)
 
