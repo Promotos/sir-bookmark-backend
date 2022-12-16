73ff2e098b1e4ce0d6344a4eafad3a88fea16293
Promotos <promotos@gmx.de>
2022-12-16 21:54:18 +0100

    pretty print

--- src/main/kotlin/de/promotos/sirbookmark/controller/BaseController.kt
+++ src/main/kotlin/de/promotos/sirbookmark/controller/BaseController.kt
@@ -13,18 +13,18 @@ import org.springframework.web.bind.annotation.ModelAttribute
 class BaseController {
 
     @Autowired
-    var buildProperties: BuildProperties? = null
+    lateinit var buildProperties: BuildProperties
 
     /**
      * Return the version number defined in the build gradle file.
      */
     @ModelAttribute("version")
     fun getVersion(): String {
-        return buildProperties!!.version
+        return buildProperties.version
     }
 
     @ModelAttribute("buildTimestamp")
     fun getBuildTimestamp(): String {
-        return buildProperties!!.time.toString()
+        return buildProperties.time.toString()
     }
 }
