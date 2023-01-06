24cae42527cc2f1293b07a64629613975141db0f
Promotos <promotos@gmx.de>
2023-01-06 21:13:23 +0100

    add comments.

--- src/main/kotlin/de/promotos/sirbookmark/SBApplication.kt
+++ src/main/kotlin/de/promotos/sirbookmark/SBApplication.kt
@@ -6,6 +6,9 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class DemoApplication
 
+/**
+ * Start the Sir-Bookmark application and provide the command line args [args] to the spring application.
+ */
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
